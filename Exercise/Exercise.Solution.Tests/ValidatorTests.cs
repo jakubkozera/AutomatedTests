a92213899c81a03627148df8bcab4103dd217b38
PC\Jakub <jakubkozera@outlook.com>
2021-09-23 07:06:24 +0200

    Member data

--- Exercise/Exercise.Solution.Tests/ValidatorTests.cs
+++ Exercise/Exercise.Solution.Tests/ValidatorTests.cs
@@ -32,16 +32,44 @@ namespace Exercise.Solution.Tests
             },
         };
 
+        private static IEnumerable<object[]> GetRangeList()
+        {
+            yield return new object[]
+            {
+                new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 1), new DateTime(2020, 1, 15)),
+                    new DateRange(new DateTime(2020, 2, 1), new DateTime(2020, 2, 15)),
+                }
+            };
+            yield return new object[]
+            {
+                new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 15), new DateTime(2020, 1, 25)),
+                },
+            };
+            yield return new object[]
+            {
+                 new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 8), new DateTime(2020, 1, 25)),
+                },
+            };
+            yield return new object[]
+            {
+                new List<DateRange>()
+                {
+                    new DateRange(new DateTime(2020, 1, 12), new DateTime(2020, 1, 14)),
+                },
+            };
+        }
 
         [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
-        public void ValidateOverlapping_ForOverlappingDateRanges_ReturnsFalse(int index)
+        [MemberData(nameof(GetRangeList))]
+        public void ValidateOverlapping_ForOverlappingDateRanges_ReturnsFalse(List<DateRange> ranges)
         {
             // arrange
-            List<DateRange> ranges = _rangeList[index];
             DateRange input = new DateRange(new DateTime(2020, 1, 10), new DateTime(2020, 1, 20));
             Validator validator = new Validator();
 
--- MyProject.Tests/MetricBmiCalculatorTests.cs
+++ MyProject.Tests/MetricBmiCalculatorTests.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MyProject.Tests
 {
     public class MetricBmiCalculatorTests
     {
+        public static IEnumerable<object[]> GetSampleData()
+        {
+            yield return new object[] { 100, 170, 34.6 };
+            yield return new object[] { 57, 170, 19.72 };
+            yield return new object[] { 70, 170, 24.22 };
+            yield return new object[] { 77, 160, 30.08 };
+            yield return new object[] { 80, 190, 22.16 };
+            yield return new object[] { 90, 190, 24.93 };
+        }
+
         [Theory]
-        [InlineData(100, 170, 34.6)]
-        [InlineData(57, 170, 19.72)]
-        [InlineData(70, 170, 24.22)]
-        [InlineData(77, 160, 30.08)]
-        [InlineData(80, 190, 22.16)]
-        [InlineData(90, 190, 24.93)]
+        [MemberData(nameof(GetSampleData))]
         public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight, double height, double bmiResult)
         {
             // arrange
