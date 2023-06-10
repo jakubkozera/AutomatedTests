4a881c4f5967e668e8bfe0a628dff71da0302615
Jakub Kozera <kozerakuba@gmail.com>
2023-06-10 13:27:48 +0200

    bugfix

--- Exercise/Exercise/2/ListHelper.cs
+++ Exercise/Exercise/2/ListHelper.cs
@@ -10,8 +10,6 @@ namespace Exercise
     {
         public static List<int> FilterOddNumber(List<int> listOfNumbers)
         {
-            List<int> result = new List<int>();
-
             for (int i = 0; i < listOfNumbers.Count; i++)
             {
                 if (listOfNumbers[i] % 2 == 0)
@@ -19,7 +17,7 @@ namespace Exercise
                     listOfNumbers.RemoveAt(i);
                 }
             }
-            return result;
+            return listOfNumbers;
         }
     }
 }
